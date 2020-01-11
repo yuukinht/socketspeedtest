434f8e01b228dde2426bbd37a57197d44a5836fb
yuukinht <50871323+yuukinht@users.noreply.github.com>
2020-01-11 17:35:56 +0700

    Update particles.js

--- public/particles.js
+++ public/particles.js
@@ -1,6 +1,6 @@
 var socket;
 function setup() {
-	socket = io.connect('http://localhost:3000');
+	socket = io();
     socket.on('test', function(data) {
 		console.log(data.time + " - " + Date.now());		
     });	
